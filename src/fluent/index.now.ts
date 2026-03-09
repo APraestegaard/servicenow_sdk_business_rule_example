97087cdb6385399d206d5f11107cb75dac25bbd2
Anders Præstegaard <apr@mjolner.dk>
2026-03-09 07:11:39 +0100

    Business rule added

--- src/fluent/index.now.ts
+++ src/fluent/index.now.ts
@@ -1,1 +1,15 @@
 //Add your Fluent APIs here and in other now.ts files under src/fluent
+import { BusinessRule } from '@servicenow/sdk/core'
+import { businessRuleProcess } from '../server/br-rule-module'
+
+BusinessRule({
+    $id: Now.ID['br_log_priority'],
+    name: 'Log Priority',
+    description: 'Logs the priority of the current task.',
+    table: 'incident',
+    when: 'before',
+    script: businessRuleProcess,
+    //action: ['insert', 'update'],
+    active: true,
+    order: 100
+})
